995d18aa6530a154928caef6404c70b52f81c29b
“Stanislav <gorokht6mail.ru@gmail.com>
2024-01-23 22:45:06 +0200

    feat: change create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -2,9 +2,9 @@ import fs from "fs/promises";
 
 const create = async () => {
     try {
-        await fs.writeFile("files/fresh.txt", "I am fresh and young", { encoding: "utf-8", flag: "wx" });
-    } catch {
-        throw new Error("FS operation failed");
+        await fs.writeFile("./src/fs/files/fresh.txt", "I am fresh and young", { encoding: "utf-8", flag: "wx" });
+    } catch (error) {
+        throw new Error ("FS operation failed");
     }
 };
 
