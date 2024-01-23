3642df68346739e38ebea5f385dcaace5f5a7165
“Stanislav <gorokht6mail.ru@gmail.com>
2024-01-23 23:06:30 +0200

    feat: create copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,11 @@
+import fs from "fs/promises";
+
 const copy = async () => {
-    // Write your code here 
+    try {
+        await fs.cp("./src/fs/files", "./src/fs/files_copy", { errorOnExist: true, recursive: true, force: false });
+    } catch {
+        throw new Error ("FS operation failed");
+    }
 };
 
 await copy();
