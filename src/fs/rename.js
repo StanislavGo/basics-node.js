b6911c583c17269b5050c37fdf07e3332be78ac4
“Stanislav <gorokht6mail.ru@gmail.com>
2024-01-24 23:08:01 +0200

    feat: create rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,14 @@
+import fs from "fs/promises";
+
+const wrongFile = "./src/fs/files/wrongFilename.txt";
+const correctFile = "./src/fs/files/properFilename.md";
+
 const rename = async () => {
-    // Write your code here 
+    try {
+        await fs.rename(wrongFile, correctFile);
+    } catch (error) {
+        throw new Error ("FS operation failed");
+    }
 };
 
 await rename();
