d222fb01f5d5821573f6bdb4c0d4f380272fc4dd
“Stanislav <gorokht6mail.ru@gmail.com>
2024-01-25 22:02:27 +0200

    feat: create delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,11 @@
+import fs from "fs/promises";
+
 const remove = async () => {
-    // Write your code here 
+    try {
+        fs.rm("./src/fs/files/fileToRemove.txt");
+    } catch {
+        throw new Error("FS operation failed");
+    }
 };
 
 await remove();
