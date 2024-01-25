e781fbb781282c27143d93e1411a75815edd2f5c
“Stanislav <gorokht6mail.ru@gmail.com>
2024-01-25 22:21:49 +0200

    feat: create read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,14 @@
+import fs from "fs/promises";
+
+const pathToFile = "./src/fs/files/fileToRead.txt";
+
 const read = async () => {
-    // Write your code here 
+   try {
+        const content = await fs.readFile(pathToFile, { encoding: "utf8" });
+        console.log(content);
+   } catch (error) {
+        throw new Error("FS operation failed");
+   }
 };
 
 await read();
