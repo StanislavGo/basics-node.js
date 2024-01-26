6d448b1aa0e3e8946a9ba624cb5fc8c88cd93c0b
“Stanislav <gorokht6mail.ru@gmail.com>
2024-01-26 20:41:30 +0200

    feat: create function to parse command line arguments

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+    const allArgs = process.argv.slice(2);
+    const resultArr = [];
+    
+    while (allArgs.length > 0) {
+        const argName = allArgs.shift().slice(2);
+        const argValue = allArgs.shift();
+
+        resultArr.push(`${argName} is ${argValue}`);
+    }
+
+    const stringifiedArgs = resultArr.join(", ");
+    console.log(stringifiedArgs);
 };
 
 parseArgs();
