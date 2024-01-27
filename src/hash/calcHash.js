0c234e896801e7d8ff43cc66095a5de7cb3bc99a
“Stanislav <gorokht6mail.ru@gmail.com>
2024-01-27 22:58:39 +0200

    feat: create function to calculate hash of a file

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,12 @@
+import fs from "fs/promises";
+import { createHash } from "crypto";
+
+const pathToFile = "./src/hash/files/fileToCalculateHashFor.txt";
+
 const calculateHash = async () => {
-    // Write your code here 
+    const fileInfo = await fs.readFile(pathToFile);
+    const calcHash = createHash("sha256").update(fileInfo).digest("hex");
+    console.log(calcHash);
 };
 
 await calculateHash();
