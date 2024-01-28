ef701d225c98639d8617d0d6e7988767751754eb
“Stanislav <gorokht6mail.ru@gmail.com>
2024-01-29 00:17:45 +0200

    feat: create function that decompresses a file

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import { createReadStream, createWriteStream } from "fs";
+import { createGunzip } from "zlib";
+import { pipeline } from "stream";
+
+const inputStream = createReadStream("./src/zip/files/archive.gz");
+const outputStream = createWriteStream("./src/zip/files/fileToCompress.txt");
+const gunzipStream = createGunzip();
+
 const decompress = async () => {
-    // Write your code here 
+    pipeline(
+        inputStream,
+        gunzipStream,
+        outputStream,
+        (error) => console.log(error)
+    );
 };
 
 await decompress();
