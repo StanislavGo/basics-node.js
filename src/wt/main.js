a0cfbadb0d71efcee45b3692af9caa7aeda25375
“Stanislav <gorokht6mail.ru@gmail.com>
2024-01-30 00:44:26 +0200

    feat: create function to work with worker_threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,28 @@
+import { cpus } from "os";
+import { Worker } from "worker_threads";
+
+const pathToWorkerFile = "./src/wt/worker.js";
+const numberOfCores = cpus().length;
+const start_number = 10;
+
+const workerService = (number) => new Promise(resolve => {
+    const worker = new Worker(pathToWorkerFile, { workerData: number });
+
+    worker.on("message", data => resolve({
+        status: "resolved",
+        data
+    }));
+
+    worker.on("error", error => resolve({
+        status: "error",
+        data: null
+    }));
+});
+
 const performCalculations = async () => {
-    // Write your code here
+    const workers = Array.from({ length: numberOfCores }, (_, i) => workerService(start_number + i));
+    const result = await Promise.all(workers);
+    console.log(result);
 };
 
 await performCalculations();
